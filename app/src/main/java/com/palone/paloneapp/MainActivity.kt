747bd750a2dbb4b7a5e9f5553830d93d62674cf5
UsernameVestiisnotavailable <vesti_xen@protonmail.com>
2022-10-23 13:43:12 +0200

    test commit

--- app/src/main/java/com/palone/paloneapp/MainActivity.kt
+++ app/src/main/java/com/palone/paloneapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text(text = "test")
+                    Text(text = "testt")
                 }
             }
         }
